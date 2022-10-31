891d40b7269fe27c90dba257e52567e8efaa6d74
artpi <artur.piszek@gmail.com>
2022-10-31 19:19:18 +0100

    Change action

--- transcribe.sh
+++ transcribe.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-youtube-dl -f 250 --print-json -a inbox.md > output.json
+youtube-dl -v -f 250 --print-json -a inbox.md > output.json
 filename=$(jq -r "._filename"  < ./output.json)
+echo "Transcribing ${filename}"
 whisper -o ./transcriptions "./${filename}"
