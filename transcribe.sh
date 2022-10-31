82940b2aa4a7637938e7a751129dcdd7c1d2b0b1
artpi <artur.piszek@gmail.com>
2022-10-31 20:52:56 +0100

    Better handling

--- transcribe.sh
+++ transcribe.sh
@@ -4,7 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-youtube-dl -v -f 250 --print-json -a inbox.md > output.json
+youtube-dl -v -f 250 --print-json $1 > output.json
 filename=$(jq -r "._filename"  < ./output.json)
 echo "Transcribing ${filename}"
 whisper -o ./transcriptions "./${filename}"
+cp "./transcriptions/${filename}.txt" ./output.txt
